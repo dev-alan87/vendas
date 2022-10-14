90a2b43f5b217aad6d174133f0edac1c572ca730
Alan Alves <dev.alan87@gmail.com>
2022-10-14 16:43:41 -0300

    Defined JPA Entity Client

--- src/main/java/io/github/dev_alan87/sales/domain/entity/Client.java
+++ src/main/java/io/github/dev_alan87/sales/domain/entity/Client.java
@@ -1,9 +1,19 @@
 package io.github.dev_alan87.sales.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Column(length = 1000, nullable = false)
 	private String name;
 	
 	public Client() { }
