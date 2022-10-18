c7a5cdc986ea22f9f30b669cb054a7a6d93f2d8c
Alan Alves <dev.alan87@gmail.com>
2022-10-18 17:59:15 -0300

    Change Product class as a Entity

--- src/main/java/io/github/dev_alan87/sales/domain/entity/Product.java
+++ src/main/java/io/github/dev_alan87/sales/domain/entity/Product.java
@@ -2,10 +2,26 @@ package io.github.dev_alan87.sales.domain.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "description", length = 1000, nullable = false)
 	private String description;
+	
+	@Column(name = "price")
 	private BigDecimal price;
 	
 	public Integer getId() {
