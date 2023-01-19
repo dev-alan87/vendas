831d86e66862a73dc4b5aad9d81871e0d1d8b171
Alan Alves <dev.alan87@gmail.com>
2023-01-18 23:20:48 -0300

    configured pom.xml for deploying producting or development

--- src/main/java/io/github/dev_alan87/sales/SalesApplication.java
+++ src/main/java/io/github/dev_alan87/sales/SalesApplication.java
@@ -2,14 +2,16 @@ package io.github.dev_alan87.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SalesApplication {
+public class SalesApplication
+        extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
 
 }
