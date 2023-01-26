f354bf5f4cb79c98e599c5190bacaacf03aff4cd
Alan Alves <dev.alan87@gmail.com>
2023-01-26 16:39:22 -0300

    updated with WebSecurity not deprecated methods

--- src/main/java/io/github/dev_alan87/sales/config/SecurityConfig.java
+++ src/main/java/io/github/dev_alan87/sales/config/SecurityConfig.java
@@ -1,27 +1,26 @@
 package io.github.dev_alan87.sales.config;
 
+import io.github.dev_alan87.sales.security.jwt.JwtAuthFilter;
+import io.github.dev_alan87.sales.security.jwt.JwtService;
+import io.github.dev_alan87.sales.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.github.dev_alan87.sales.security.jwt.JwtAuthFilter;
-import io.github.dev_alan87.sales.security.jwt.JwtService;
-import io.github.dev_alan87.sales.service.impl.UserServiceImpl;
-
 @EnableWebSecurity
-public class SecurityConfig
-        extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired @Lazy
     private UserServiceImpl userService;
@@ -39,16 +38,16 @@ public class SecurityConfig
         return new JwtAuthFilter(jwtService, userService);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(userService)
-            .passwordEncoder(passwordEncoder());
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration
+                .getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
             .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/api/products/**").hasRole("ADMIN")
@@ -60,12 +59,14 @@ public class SecurityConfig
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
     
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return (web) ->
+                web.ignoring().antMatchers(
                     "/v2/api-docs",
                     "/configuration/ui",
                     "/swagger-resources/**",
